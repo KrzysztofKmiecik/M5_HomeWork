196eb13af4ce2b8c0cb2586b267d386d790c01bb
KrzysztofKmiecik1975 <krzysztofkmiecik1975@gmail.com>
2021-05-30 12:40:27 +0200

    Initial commit

---
+++ src/main/java/pl/kmiecik/m5_homework/M5HomeWorkApplication.java
@@ -0,0 +1,13 @@
+package pl.kmiecik.m5_homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M5HomeWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M5HomeWorkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/kmiecik/m5_homework/M5HomeWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kmiecik.m5_homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M5HomeWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
